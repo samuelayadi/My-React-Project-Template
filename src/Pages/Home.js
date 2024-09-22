2ba75e0317f6804577c1fbdb8eea4c30bac063f5
samuelayadi <samuelayadi4999@gmail.com>
2024-09-22 13:29:26 +0100

    Changes made

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <div className="bg-black text-white flex flex-1">Homecoming</div>
     </>
   );
 };
